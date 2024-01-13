785b93056f2cd8cb7d1c351f8e9d797f62406c01
Rafe McDonald <phidelityaudio@gmail.com>
2024-01-12 16:33:53 -0800

    lidar spins on off booh yeah!

--- raspberry_pi/gpio_led_test.py
+++ raspberry_pi/gpio_led_test.py
@@ -3,18 +3,21 @@
     
 Basic orientation testing and explorations to the Raspberry Pi Gpio interface
 
-    
+ this script works but the unusual item is the measurement across the ground from pin 6 ( 3rd pin on right column)
+ to pin 11 (ID GPIO 17)   is opposite to my expectation.  Zero voltage is measured during what is labeled as the on phase
+ an 3.3 (approx) during the off phase (aka when thme message in the console prints "Hi rafe ...")
 """
 
-from gpiozero import LED
+#from gpiozero import LED
+import gpiozero as gp
 from time import sleep
-
-led = LED(17)
+output_5v = gp.DigitalOutputDevice(2)
+led = gp.LED(17)
 
 while True:
     led.on()
     sleep(3)
-    print("ON\n")
+    print("ON for 3 s \n")
     led.off()
     sleep(3)
-    print("hi Rafe - doesnt seem like 3 seconds does it")
+    print("hi - led.off call for 3 seconds\n\n[]")
