efd29da60b96e2d2f888957b14aca91fc5d8ea39
Rafe McDonald <phidelityaudio@gmail.com>
2024-01-16 19:50:01 -0800

    basic attempt with UART declaration

--- raspberry_pi/simple_use.py
+++ raspberry_pi/simple_use.py
@@ -1,15 +1,23 @@
 from adafruit_rplidar import RPLidar
 import os
 from math import floor
-from busio import UART
+import gpiozero as gp
+import busio as bu
 
 # for use with serial port usb
-# PORT_NAME  = '/dev/ttyUSB0'
+PORT_NAME  = '/dev/ttyUSB0'
 
 # attempt with our uart  this through an error as I was attempting to configure the tx rx ports 
-#uart = busio.UART(2,)
+gp_transmit = gp.DigitalOutputDevice(14) #  gpio / BCM numbering defualt tx pin
+gp_receive = gp.DigitalInputDevice(15) # default /Bcm rx pin
+baud_rate = 115200
+uart = bu.UART(rx = gp_transmit, tx = gp_receive, baudrate = baud_rate)
+lidar = RPLidar(uart)
 
-lidar = RPLidar('/dev/ttyUSB0')
+#PORT_NAME = '/dev/ttyUSB0'
+
+#lidar = RPLidar('/dev/ttyUSB0')
+#lidar = RPLidar(None, PORT_NAME, timeout=3 )
 
 info = lidar.get_info()
 print(info)
