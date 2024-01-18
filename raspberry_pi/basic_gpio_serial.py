3a8a66d9f7096a4cd9bf299822942356b2369463
Rafe McDonald <phidelityaudio@gmail.com>
2024-01-18 12:57:05 -0800

    testing polling the tx rx pipelines

--- raspberry_pi/basic_gpio_serial.py
+++ raspberry_pi/basic_gpio_serial.py
@@ -9,23 +9,50 @@ import serial
 PORT_NAME  = '/dev/ttyUSB0'
 
 from time import sleep
-output_5v = gp.DigitalOutputDevice(2) #the arg is the pinnumber not the io
-motor_control_pwm = gp.DigitalOutputDevice(13)
+# gpio standard for pin declaration
+dc_pin = 2
+motor_pin = 13
+tx_pin = 14
+rx_pin = 15
+
+output_5v = gp.DigitalOutputDevice(dc_pin) #the arg is the pinnumber not the io
+motor_control_pwm = gp.DigitalOutputDevice(motor_pin)
+
 
 # attempt with our uart  this through an error as I was attempting to configure the tx rx ports 
-gp_transmit = gp.DigitalOutputDevice(14) #  gpio / BCM numbering defualt tx pin
-gp_receive = gp.DigitalInputDevice(15) # default /Bcm rx pin
+gp_transmit = gp.DigitalOutputDevice(tx_pin) #  gpio / BCM numbering defualt tx pin
+gp_receive = gp.DigitalInputDevice(rx_pin) # default /Bcm rx pin
 
 output_5v.on()
 motor_control_pwm.on()
 
-motor_pin = 13 #gpio labelling
 baud_rate = 115200
 port_serial = '/dev/ttyS0'
 ser = serial.Serial(port=port_serial, baudrate=baud_rate, timeout=3)
+sleep(5)
+lidar = RPLidar(motor_pin=motor_pin, port=port_serial, timeout=3)
+
+try: 
+    while True:
+        #read gpio input
+        gp_in_data = gp_receive.value
+        print(f"this is the type of gp input data object {type(gp_in_data)}\n")
+        print(f"this is the data {gp_in_data}\n")
+        ser.write(str(gp_in_data).encode())
+        
+
+        # test the tx data
+        gp_tx_data = gp_transmit.value
+        print(f"tx data {type(gp_tx_data)}\n")
+        print(f"tx data {gp_tx_data}\n")
+        ser.write(str(gp_in_data).encode())
 
 
-lidar = RPLidar('13', port=ser, timeout=3)
+except KeyboardInterrupt:
+    pass
+
+
+lidar = RPLidar(motor_pin=motor_pin, port=port_serial, timeout=3)
 
 
 # bleow are some basic method calls as tests 
---
+++ raspberry_pi/ser_rplidar.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python3
+# This is John Curry's adaptation
+
+import serial
+import sys
+import json
+import click
+import itertools
+
+def try_parse_line(ser):
+    line = None
+    try:
+        l = ser.readline().decode('utf-8').rstrip()
+        json.loads(l)
+        line = l
+    except KeyboardInterrupt:
+        raise
+    except:# json.decoder.JSONDecodeError:
+        pass
+
+    return line
+
+def WaitingLine():
+    dots = itertools.cycle(['.    ', '..   ', '...  ', '.... ', '.....'])
+    slashes = itertools.cycle(['\\', '|', '/', '-'])
+    while True:
+        yield ('\r' + next(slashes) + next(dots))
+
+
+def debug_read(ser):
+    try:
+        waiting = False
+        waitingLine = WaitingLine()
+        while True:
+            line = ser.readline().decode('utf-8').rstrip()
+
+            if line and len(line) > 0:
+                if waiting: 
+                    sys.stdout.write("\r")
+                    waiting = False
+                    waitingLine = WaitingLine()
+
+                sys.stdout.write(line + "\n")
+            else:
+                waiting = True
+                sys.stdout.write(next(waitingLine))
+
+    except KeyboardInterrupt:
+        print("Exiting...")
+        sys.exit(0)
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+def default_read(ser):
+        print("Starting to read data from serial port...")
+        print("Press Ctrl+C to stop reading...")
+        doc = "["
+        running = True
+        try:
+            i = 0
+            while True:
+                if line := try_parse_line(ser):
+                    i += 1
+                    sys.stdout.write("Number read: \t\r%d" % i)
+                    line += ",\n"
+                    doc += line
+                    if not running:
+                        line += "]"
+                        break
+
+        except KeyboardInterrupt:
+            if not running:
+                raise
+            else:
+                running = False
+
+        except Exception as e:
+            print(e)
+            sys.exit(1)
+
+        with open("data.json", "w") as f:
+            print("Writing to file...")
+            f.write(doc)
+            print("Done!!!")
+            sys.exit(0)
+
+@click.command()
+@click.option('--debug', is_flag=True, default=False, help='debug')
+def main(debug):
+    with serial.Serial('/dev/ttyS0', 115200, timeout=1) as ser:
+        if (debug):
+                debug_read(ser)
+
+        else:
+            default_read(ser)
+
+if __name__ == "__main__":
+    main()
